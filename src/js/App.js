1ad8b68f8b117348763fc1986161fea6d9d8737b
m2v <69117810+iuvoJS@users.noreply.github.com>
2021-01-27 11:40:13 +0100

    started with apply figma file

--- src/js/App.js
+++ src/js/App.js
@@ -1,159 +1,58 @@
-import $ from "jquery";
+// import $ from "jquery";
+
 import { Component } from "react";
 import platform from "platform";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faBook,
-  faGraduationCap,
-  faDownload,
-  faGamepad,
-  faNewspaper,
-  faHeadset,
-  faHome,
-} from "@fortawesome/free-solid-svg-icons";
+faGithub,
+faDiscord,
+faPatreon,
+faFacebookF,
+} from "@fortawesome/free-brands-svg-icons";
+import Iphone from "./iphone";
 
-class Navbar extends Component {
-  $code = () => {
-    $(".menu-toggle").click(function (e) {
-      if ($(this).hasClass("open") || $("body").hasClass("show-sidebar")) {
-        $(this).removeClass("open");
-        $("body").removeClass("show-sidebar");
-      } else {
-        $(this).addClass("open");
-        $("body").addClass("show-sidebar");
-      }
-
-      e.preventDefault();
-    });
-
-    $(".nav-backdrop").mouseup(() => {
-      $(".menu-toggle").removeClass("open");
-      $("body").removeClass("show-sidebar");
-    });
-  };
-
+class Main extends Component {
   componentDidMount() {
-    this.$code();
+    console.log(platform.os.family);
   }
 
   render() {
     return (
-      <div>
-        <nav className="navbar-expand navbar">
-          <div className="collapse navbar-collapse">
-            <div className="nav-wrapper">
-              <span className="menu-toggle">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-              </span>
+      <div className="main">
+        <div className="row">
+          <div className="col-lg-3">
+            <Iphone path="iphone-bg.png" />
+          </div>
+          <div className="col-lg-6 content">
+            <h1>Download now!</h1>
+            <h3>Lorem ipsum dolor sit amet</h3>
+            <div className="button-group">
+              <button className="btn primary">Download</button>
+              <button className="btn secondary">Read more</button>
             </div>
           </div>
-        </nav>
-        <div className="nav-backdrop" tabIndex="-1"></div>
+        </div>
+        <div className="socials">
+          <div className="icon">
+            <FontAwesomeIcon icon={faGithub} />
+          </div>
+          <div className="icon">
+            <FontAwesomeIcon icon={faDiscord} />
+          </div>
+          <div className="icon">
+            <FontAwesomeIcon icon={faPatreon} />
+          </div>
+          <div className="icon">
+            <FontAwesomeIcon icon={faFacebookF} />
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-class Sidenav extends Component {
-  $code = (e) => {
-    console.log(e);
-  };
-
-  componentDidMount() {
-    this.$code();
-  }
-
-  render() {
-    return (
-      <aside className="sidebar">
-        <div className="side-inner">
-          <div></div>
-          <div className="nav-menu">
-            <ul>
-              <li className="active">
-                <a href="#!">
-                  <FontAwesomeIcon icon={faHome} />
-                  Yeeet It Out!
-                </a>
-              </li>
-              <li>
-                <a href="#!">
-                  <FontAwesomeIcon icon={faDownload} />
-                  Download
-                </a>
-              </li>
-              <li>
-                <a href="#!">
-                  <FontAwesomeIcon icon={faGraduationCap} />
-                  Learn more
-                </a>
-              </li>
-              <li>
-                <a href="#!">
-                  <FontAwesomeIcon icon={faBook} />
-                  Documentation
-                </a>
-              </li>
-            </ul>
-            <hr className="m-0"></hr>
-            <ul>
-              <li>
-                <a href="#!">
-                  <FontAwesomeIcon icon={faGamepad} />
-                  Games
-                </a>
-              </li>
-              <li>
-                <a href="#!">
-                  <FontAwesomeIcon icon={faNewspaper} />
-                  News / Events
-                </a>
-              </li>
-              <li>
-                <a href="#!">
-                  <FontAwesomeIcon icon={faHeadset} />
-                  Support
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </aside>
-    );
-  }
-}
-
-class Header extends Component {
-  componentDidMount() {
-    console.log(platform.os.family)
-  }
-
-  render() {
-    return (
-      <header>
-        <div className="container">
-          <h1>Yeeet It Out!</h1>
-          <div className="button-group">
-            <a href="#!" className="btn btn-secondary">play demo</a>
-            <a href="#!" className="btn btn-primary">get now</a>
-          </div>
-        </div>
-      </header>
-    );
-  }
-}
-
 function App() {
-  return (
-    <div>
-      <Sidenav />
-      <Navbar />
-      <Header />
-    </div>
-  );
+  return <Main />;
 }
 
 export { App };
---
+++ src/js/iphone.js
@@ -0,0 +1,61 @@
+import { Component } from "react";
+import path from "path";
+
+class Iphone extends Component {
+  render() {
+    return (
+      <svg
+        data-name="Ebene 1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 443.4 907.6"
+      >
+        <defs>
+          <style
+            dangerouslySetInnerHTML={{
+              __html:
+                "\n            .cls-1,\n            .cls-3,\n            .cls-6 {\n                fill: none;\n            }\n\n            .cls-2 {\n                fill: #fff;\n            }\n\n            .cls-3 {\n                stroke: #f7f7f7;\n                stroke-width: 4px;\n            }\n\n            .cls-3,\n            .cls-6 {\n                stroke-miterlimit: 10;\n            }\n\n            .cls-4 {\n                clip-path: url(#clip-path);\n            }\n\n            .cls-5 {\n                isolation: isolate;\n            }\n\n            .cls-6 {\n                stroke: #e7eaf3;\n            }\n\n            .cls-7 {\n                fill: #f7f7f7;\n            }\n        ",
+            }}
+          />
+          <clipPath id="clip-path">
+            <path
+              className="cls-1"
+              d="M333.4,30.4h0a7.37,7.37,0,0,1,7.2-7.5H376c26.9,0,48.8,22.7,48.8,50.7V838.8c0,28-21.8,50.7-48.8,50.7H67.4c-26.9,0-48.8-22.7-48.8-50.7V73.6c0-28,21.8-50.7,48.8-50.7h35.3a7.37,7.37,0,0,1,7.2,7.5h0c0,14.9,11.6,27,26,27H307.4C321.8,57.5,333.4,45.4,333.4,30.4Z"
+            />
+          </clipPath>
+        </defs>
+        <path
+          className="cls-2"
+          d="M378.3,0H65.1A65,65,0,0,0,0,65.1V842.5a65.1,65.1,0,0,0,65.1,65.1H378.3a65.1,65.1,0,0,0,65.1-65.1V65.1A65.1,65.1,0,0,0,378.3,0Z"
+        />
+        <path
+          className="cls-3"
+          d="M242.8,36.8H196.4a3.59,3.59,0,0,1-3.6-3.6h0a3.59,3.59,0,0,1,3.6-3.6h46.4a3.59,3.59,0,0,1,3.6,3.6h0A3.59,3.59,0,0,1,242.8,36.8Z"
+        />
+        <circle className="cls-3" cx="269.5" cy="33.2" r="5.5" />
+        <g className="cls-4">
+          <image
+            id="SVGiphoneX1Img1"
+            className="cls-5"
+            width={407}
+            height={867}
+            transform="translate(18.19 22.71)"
+            xlinkHref={path.join(__dirname, this.props.path)}
+          />
+        </g>
+        <path
+          id="iphoneXID_1"
+          data-name="iphoneXID 1"
+          className="cls-6"
+          d="M333.4,30.4h0a7.37,7.37,0,0,1,7.2-7.5H376c26.9,0,48.8,22.7,48.8,50.7V838.8c0,28-21.8,50.7-48.8,50.7H67.4c-26.9,0-48.8-22.7-48.8-50.7V73.6c0-28,21.8-50.7,48.8-50.7h35.3a7.37,7.37,0,0,1,7.2,7.5h0c0,14.9,11.6,27,26,27H307.4C321.8,57.5,333.4,45.4,333.4,30.4Z"
+        />
+        <path
+          className="cls-7"
+          d="M285.2,879.8h-127a2.65,2.65,0,0,1-2.6-2.6h0a2.65,2.65,0,0,1,2.6-2.6h127a2.65,2.65,0,0,1,2.6,2.6h0A2.58,2.58,0,0,1,285.2,879.8Z"
+        />
+      </svg>
+    );
+  }
+}
+
+export default Iphone;
